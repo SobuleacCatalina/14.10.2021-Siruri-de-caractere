6ff41a9f2870db8fe1e0b33292371412ddfad00e
SobuleacCatalina <71014246+SobuleacCatalina@users.noreply.github.com>
2021-10-14 17:21:34 +0300

    Add files via upload

---
+++ exercitiul_8.py
@@ -0,0 +1,22 @@
+sir=str(input("Introduceti sirul"))
+sir1=""
+sir2=""
+sir3=""
+for i in sir:
+    if (ord(i)>=65)and(ord(i)<=89):
+        sir1+=chr(ord(i)+1)
+    else:
+        sir1+=i
+print("a) Inlocuieste litele de la A pana la Y cu urmatoarea litera: \n",sir1)
+for i in sir:
+    if i=='Z':
+        sir2+="A"
+    else:
+        sir2+=i
+print("b) Inlocuieste litele de Z cu A: \n",sir2)
+for i in sir:
+    if i==' ':
+        sir3+="-"
+    else:
+        sir3+=i
+print("c) Inlocuieste spatiu cu - : \n",sir3)
